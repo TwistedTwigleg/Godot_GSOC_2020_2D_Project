e33fae582c65a36fc5e763fcbc2f489204fd3cbe
pwab <philipp.wabnitz@mb.tu-chemnitz.de>
2020-09-05 15:44:57 +0200

    Added a fully working RR-Comparison and did a massive refactor of the code
    I could clean some code by simply deleting now unused variables. Also the RRComparison seems to be working fine apart from a sometimes disapearing TwoBoneIK.
    SkeletonModification2DSlider is still not able to work (see issue #2) but I had forgot to push the project file which contains a reference to the class.

--- Scenes/Test_RRComparison/RR_target.gd
+++ Scenes/Test_RRComparison/RR_target.gd
@@ -1,25 +1,24 @@
 extends Position2D
 
-# HACK: changed export to nothing
-#@export var tip_node: NodePath
-var tip_node: NodePath
+var target_nodes
 @onready var active = false
 
-### HACK
 func _ready():
-	print(self.name)
-	if self.name == "RR_Robot_FABRIK_Target":
-		tip_node = NodePath("RR_Robot_FABRIK/Bone2D_A0A/Bone2D_AB/Tip")
-### END OF HACK
+	target_nodes = [
+		get_parent().get_node("RR_FABRIK_Target"),
+		get_parent().get_node("RR_CCDIK_Target"),
+		get_parent().get_node("RR_TwoBoneIK_Target")
+	]
 
 func _process(delta):
 	if active:
-		self.position = get_global_mouse_position()
+		var position_delta = get_global_mouse_position() - self.global_position
+		for target in target_nodes:
+			target.position += position_delta
 
 func _input(event):
 	if active:
 		if event is InputEventMouseButton and not event.is_pressed():
-			print("Released on ", self.name)
 			active = false
 
 func _on_Tip_input_event(viewport, event, shape_idx):
